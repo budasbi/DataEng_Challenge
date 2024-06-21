108839be920ccd1678ca8350aeaf80d7a862bf10
OscarS <oscar.budasbi@gmail.com>
2024-06-20 21:34:06 -0500

    rds network working

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,19 +1,40 @@
 // Create a VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  
+  tags = {
+    Name = var.default_tag
+  }
+}
+
+# Creación de un Internet Gateway
+resource "aws_internet_gateway" "igw_rds" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = var.default_tag
+  }
 }
 
 // Create a Subnet
-resource "aws_subnet" "subnet_a" {
+resource "aws_subnet" "public_subnet_a" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = var.default_tag
+  }
 }
 
-resource "aws_subnet" "subnet_b" {
+resource "aws_subnet" "private_subnet_b" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  tags = {
+    Name = var.default_tag
+  }
 }
 
 resource "aws_security_group" "rds_sg" {
@@ -21,12 +42,36 @@ resource "aws_security_group" "rds_sg" {
   name_prefix = "rds-sg"
 
   vpc_id = aws_vpc.my_vpc.id
-
-
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = var.default_tag
+  }
+}
+
+# Creación de una tabla de rutas
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = var.default_tag
+  }
+}
+
+# Asociar el Internet Gateway a la tabla de rutas
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw_rds.id
+  
+}
+
+# Asociación de la subred pública con la tabla de rutas
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+  
 }
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,10 +4,11 @@ resource "aws_db_instance" "default" {
   engine            = "postgres"
   instance_class    = "db.t3.micro"
   db_name           = "postg"
-  identifier        = "data_challenge"
+  identifier        = "datachallenge"
   engine_version    = 16.3
   username          = "postgres"
   password          = var.rds_password
+  publicly_accessible = true
   //Network Config
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
@@ -18,12 +19,15 @@ resource "aws_db_instance" "default" {
   //Enable Backups
   final_snapshot_identifier = "db-snap"
   skip_final_snapshot       = false
+  tags = {
+    Name = var.default_tag
+  }
 }
 
 resource "aws_db_subnet_group" "my_db_subnet_group" {
   name       = "my-db-subnet-group"
-  subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+  subnet_ids = [aws_subnet.public_subnet_a.id, aws_subnet.private_subnet_b.id]
   tags = {
-    Name = "subnet-group"
+    Name = var.default_tag
   }
 }
